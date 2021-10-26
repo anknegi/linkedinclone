fac710e8f575725e6a9b3aa441ccc81ac0288667
Ankit Negi <ankitnegi2008@gmail.com>
2021-10-26 22:47:04 +0530

    removed delicate information of config

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import { FirebaseApp, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 const firebaseConfig = {
-    apiKey: "AIzaSyDX9Dy6kPMtw0Sr4KpbQN3V45dV-HGbouI",
-    authDomain: "linkedin-clone-dae89.firebaseapp.com",
-    projectId: "linkedin-clone-dae89",
-    storageBucket: "linkedin-clone-dae89.appspot.com",
-    messagingSenderId: "724327000087",
-    appId: "1:724327000087:web:d3a61882de333cf626d452"
+    
   };
   const app: FirebaseApp = initializeApp(firebaseConfig);
 
